f38822ee0ffa89b4d89b4dd07acc052b716b74e3
BruhThisMans <nathan615richie@gmail.com>
2023-10-02 16:47:49 -0500

    Added the algorithm used.
    Algorithm still not working as intended. Currently searching for fix.

---
+++ Programs/Algorithm.py
@@ -0,0 +1,31 @@
+import heapq
+
+def dijkstra(graph, start, end):
+    # Initialize distances and previous nodes
+    distances = {vertex: float('infinity') for vertex in graph}
+    distances[start] = 0
+    priority_queue = [(0, start)]
+
+    while priority_queue:
+        current_distance, current_vertex = heapq.heappop(priority_queue)
+
+        if current_distance > distances[current_vertex]:
+            continue
+
+        for neighbor, weight in graph[current_vertex].items():
+            distance = current_distance + weight
+
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(priority_queue, (distance, neighbor))
+
+    path = []
+    current = end
+    while current != start:
+        path.insert(0, (current, current.buildings))
+        current = min(
+            (neighbor for neighbor, weight in graph[current].items() if distances[current] == distances[neighbor] + weight),
+            key=lambda x: distances[x]
+        )
+    path.insert(0, (start, start.buildings))
+    return path
--- Programs/CreateMap.py
+++ Programs/CreateMap.py
@@ -1,5 +1,9 @@
 from Vertex import Vertex
 from Graph import Graph
+from Algorithm import dijkstra
+
+#Init graph
+Map = Graph()
 
 #Init vertex
 A = Vertex("A")
@@ -16,11 +20,28 @@ K = Vertex("K")
 L = Vertex("L")
 M = Vertex("M")
 N = Vertex("N")
+Map.add_vertex(A)
+Map.add_vertex(B)
+Map.add_vertex(C)
+Map.add_vertex(D) 
+Map.add_vertex(E)
+Map.add_vertex(F)
+Map.add_vertex(G)
+Map.add_vertex(H)
+Map.add_vertex(I)
+Map.add_vertex(J)
+Map.add_vertex(K)
+Map.add_vertex(L)
+Map.add_vertex(M)
+Map.add_vertex(N)
 
 #A Vertex
 A.add_edge(B.value)
 A.add_edge(D.value)
 A.add_edge(E.value)
+Map.add_edge(A,B)
+Map.add_edge(A,D)
+Map.add_edge(A,E)
 A.add_building("Arthur's House","Top")
 A.add_building("Trade Post","Bottom")
 
@@ -29,19 +50,28 @@ B.add_edge(A.value)
 B.add_edge(D.value)
 B.add_edge(C.value)
 B.add_edge(E.value)
+Map.add_edge(B,A)
+Map.add_edge(B,D)
+Map.add_edge(B,C)
+Map.add_edge(B,E)
 B.add_building("Arthur's House","Left")
 B.add_building("Dutch's House","Right")
 
 #C Vertex
 C.add_edge(B.value)
+Map.add_edge(C,B)
 C.add_building("Jared's House","Left")
 C.add_building("Tom's House","Right")
 
 #D Vertex
 D.add_edge(A.value)
-D.add_edge(D.value)
+D.add_edge(B.value)
 D.add_edge(F.value)
 D.add_edge(E.value)
+Map.add_edge(D,A)
+Map.add_edge(D,B)
+Map.add_edge(D,F)
+Map.add_edge(D,E)
 D.add_building("General Store","Bottom")
 D.add_building("Dutch's House","Top")
 
@@ -50,17 +80,25 @@ E.add_edge(A.value)
 E.add_edge(D.value)
 E.add_edge(B.value)
 E.add_edge(N.value)
+Map.add_edge(E,A)
+Map.add_edge(E,D)
+Map.add_edge(E,B)
+Map.add_edge(E,N)
 E.add_building("Trade Post","Left")
 E.add_building("General Store","Right")
 
 #F Vertex 
 F.add_edge(D.value)
 F.add_edge(G.value)
+Map.add_edge(F,D)
+Map.add_edge(F,G)
 F.add_building("Sheriffs Office","Top")
 
 #G Vertex
 G.add_edge(F.value)
 G.add_edge(H.value)
+Map.add_edge(G,F)
+Map.add_edge(G,H)
 G.add_building("Courtyard","Top")
 G.add_building("Town Bank","Bottom")
 
@@ -68,17 +106,23 @@ G.add_building("Town Bank","Bottom")
 H.add_edge(G.value)
 H.add_edge(I.value)
 H.add_edge(K.value)
+Map.add_edge(H,G)
+Map.add_edge(H,I)
+Map.add_edge(H,K)
 H.add_building("Town Hall","Bottom")
 
 
 #I Vertex
 I.add_edge(H.value)
 I.add_edge(J.value)
+Map.add_edge(I,J)
+Map.add_edge(I,H)
 I.add_building("Courtyard","Left")
 I.add_building("Saloon","Right")
 
 #J Vertex
 J.add_edge(I.value)
+Map.add_edge(J,I)
 J.add_building("Horse Stable","Left")
 J.add_building("Post Office","Top")
 J.add_building("Graveyard","Right")
@@ -87,21 +131,31 @@ J.add_building("Graveyard","Right")
 #K Vertex
 K.add_edge(H.value)
 K.add_edge(L.value)
+Map.add_edge(K,H)
+Map.add_edge(K,L)
 K.add_building("Saloon","Top")
 K.add_building("Micah's House","Bottom")
 
 #L Vertex
 L.add_edge(K.value)
 L.add_edge(M.value)
+Map.add_edge(L,M)
+Map.add_edge(L,K)
 L.add_building("Hotel","Top")
 L.add_building("Emerald Ranch","Right")
 
 #M Vertex
 M.add_edge(L.value)
 M.add_edge(N.value)
+Map.add_edge(M,L)
+Map.add_edge(M,N)
 M.add_building("Army Recruitment","Top")
 
 #N Vertex
 N.add_edge(M.value)
 N.add_edge(E.value)
+Map.add_edge(N,M)
+Map.add_edge(N,E)
 N.add_building("Leviticus Cornwall Estate","Bottom")
+
+print(dijkstra(Map,"Town Hall","Emerald Ranch"))
